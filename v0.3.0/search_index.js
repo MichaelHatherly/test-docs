e71619ce222a7d0b52436d1a111b0fee09746cd9
Documenter.jl <documenter@juliadocs.github.io>
2020-10-22 09:15:05 +0000

    build based on bd4c5bc

---
+++ v0.3.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TestDocs","page":"TestDocs","title":"TestDocs","text":"","category":"section"},{"location":"","page":"TestDocs","title":"TestDocs","text":"using Dates\nnow()","category":"page"}]
+}
